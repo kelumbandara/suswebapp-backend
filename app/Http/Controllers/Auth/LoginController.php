cc3554eb820bb2c89366f02492e5bd9633c71e43
Tharaka Prasad <129380063+tharaka-prasad@users.noreply.github.com>
2025-01-17 14:33:55 +0530

    login page error fix

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -1,34 +1,40 @@
 <?php
+
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use App\Models\User;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 
 class LoginController extends Controller
 {
-
     public function login(Request $request)
     {
-          $validated = $request->validate([
+        $validated = $request->validate([
             'email' => 'required|email',
             'password' => 'required',
         ]);
 
         $user = User::where('email', $request->email)->first();
 
-        if (!$user) {
-            return response()->json(['error' => 'User not found'], 404);
+        if ($user) {
+            if ($request->password === $user->password) {
+                $token = $user->createToken('SATTVA')->plainTextToken;
+
+                return response()->json([
+                    'message' => 'Login successful',
+                    'token' => $token,
+                    'user' => $user
+                ], 201);
+            }
+
+            return response()->json([
+                'message' => 'Invalid password',
+            ], 401);
         }
 
-        if (!Hash::check($request->password, $user->password)) {
-            return response()->json(['error' => 'Password mismatch'], 401);
-        }
-
-        $token = $user->createToken('YourAppName')->plainTextToken;
-
-        return response()->json(['token' => $token], 200);
-
+        return response()->json([
+            'message' => 'User not found',
+        ], 404);
     }
 }
