d3c3e16441af44d0641027313b3a108d05df3064
Tharaka Prasad <129380063+tharaka-prasad@users.noreply.github.com>
2025-01-22 13:51:35 +0530

    date and time part

--- app/Models/S_A_InternalAudit.php
+++ app/Models/S_A_InternalAudit.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -40,5 +41,15 @@ class S_A_InternalAudit extends Model
             $maxId                       = S_A_InternalAudit::max('id') + 1;
             $internalAudit->referenceNumber = 'AUD-' . $maxId;
         });
+    }
+
+    public function setAuditDateAttribute($value)
+    {
+        $this->attributes['auditDate'] = Carbon::parse($value)->format('Y-m-d');
+    }
+
+    public function setDateApprovalAttribute($value)
+    {
+        $this->attributes['dateApproval'] = Carbon::parse($value)->format('Y-m-d');
     }
 }
