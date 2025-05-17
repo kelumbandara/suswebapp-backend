d0ca82d2d6a45e6ff72c027f3c5718a978f4bc5a
Tharaka Prasad <129380063+tharaka-prasad@users.noreply.github.com>
2025-05-17 09:30:15 +0530

    `Refactor SaCmPurchaseInventoryRecodeController: add user interface data and update published status route`

--- routes/api.php
+++ routes/api.php
@@ -257,7 +257,7 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('purchase-inventory-records/{id}/update', [SaCmPurchaseInventoryRecodeController::class, 'update']);
     Route::post('purchase-inventory-records/{id}/publish-update', [SaCmPurchaseInventoryRecodeController::class, 'publishStatus']);
     Route::delete('purchase-inventory-recode/{id}/delete', [SaCmPurchaseInventoryRecodeController::class, 'destroy']);
-    Route::get('chemical-transaction-published', [SaCmPurchaseInventoryRecodeController::class, 'publish']);
+    Route::get('chemical-transaction-published', [SaCmPurchaseInventoryRecodeController::class, 'getPublishedStatus']);
 
     Route::get('purchase-inventory-records-assign-task', [SaCmPurchaseInventoryRecodeController::class, 'assignTask']);
 });
