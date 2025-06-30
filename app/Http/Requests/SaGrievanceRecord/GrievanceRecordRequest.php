9a39ec310d5a79a2b0953bf90abae27e68dce673
Tharaka Prasad <129380063+tharaka-prasad@users.noreply.github.com>
2025-06-30 09:48:46 +0530

    `Removed 'caseId' validation rule from GrievanceRecordRequest`

--- app/Http/Requests/SaGrievanceRecord/GrievanceRecordRequest.php
+++ app/Http/Requests/SaGrievanceRecord/GrievanceRecordRequest.php
@@ -21,7 +21,6 @@ class GrievanceRecordRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'caseId'                                         => 'required|string',
             'type'                                           => 'required|string',
             'personType'                                     => 'required|string',
             'name'                                           => 'required|string',
